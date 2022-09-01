e11bcdbdb418c1262c6c98b61a4faa4023d5bf3d
Romina Caubarrere <106929242+romicaubarrere@users.noreply.github.com>
2022-09-01 09:28:36 -0300

    Add files via upload

---
+++ EjercicioOperadoresMatematicos/src/ejerciciosClase/ejercicios.java
@@ -0,0 +1,82 @@
+package ejerciciosClase;
+
+import java.util.Scanner;
+
+
+public class ejercicios {
+
+	public static void main(String[] args) {
+		
+		/*
+		Scanner sc = new Scanner (System.in);
+		
+// 2.1. Realizar las siguientes operaciones aritméticas utilizando un entorno de
+//		programación verificarlas de forma manual:
+
+
+		float numA;
+		float numB;
+		// para pedir los numeros
+		
+ comentado para no interrumpir otra accion
+
+		System.out.println("Ingrese un numero: ");
+		numA = sc.nextFloat ();
+		System.out.println("Ingrese otro numero: ");
+		numB = sc.nextFloat ();
+
+		// System.out.println("El resultado de la division es: " + (float)numA / numB);
+		
+		// System.out.println("El resultado de la multiplicacion es: " + (float)numA * numB);
+		
+		float num1 = 76;
+		float num2 = -12;
+		float num3 = -15;
+		
+		System.out.println(num1 - num2 + num3);
+				
+*/
+		// 2.2. En cada caso, decidir si la expresión matemática es verdadera o falsa:
+
+		int numA = 1;
+		int numB = 2;
+		int numC = 3;
+		int numD = 4;
+		float numE = 35;
+		float numE2 = 8/13;
+		float numInf = 1/0;
+		
+		// boolean comparaAC = numA == numB; 
+		// boolean comparaAC = numA > numB;
+		// boolean comparaAC = numA <= numB;
+		// boolean comparaAC = numA != numB;
+		
+		/*
+		 
+		
+		
+		boolean comparaAC = numE < numE2;
+		
+		
+		
+		boolean comparaAC = (numA < numB) && (numB < numC);
+		
+		
+		
+		// 2.3. Evaluar y comparar las siguientes expresiones
+		
+		assertThrows(ArithmeticException.class, () -> {
+		    int result = 1 / 0;
+		}); 
+		
+		boolean comparaAC = numA < result;
+		
+	
+		
+		System.out.println(comparaAC);
+		
+		*/
+	}
+		
+	}
+
